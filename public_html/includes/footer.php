3b49acc8e4df1085380a7548903a51cd6e2f666f
MASTER <master@brutal-chuwi.local>
2026-01-03 10:13:43 +0100

    Analytics: Integrated Google Analytics tag into global footer

--- public_html/includes/footer.php
+++ public_html/includes/footer.php
@@ -56,3 +56,13 @@
         </div>
     </div>
 </footer>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-434X450QQM"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+
+    gtag('config', 'G-434X450QQM');
+</script>
